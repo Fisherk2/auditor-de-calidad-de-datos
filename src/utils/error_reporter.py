bd3fc96fd9e1a7c58e333e38b2779c19735975e0
Fisherk2 <fisher_k2@hotmail.com>
2026-02-12 01:22:09 -0600

    utils: Genera mensajes de error para problemas de encabezdos

--- src/utils/error_reporter.py
+++ src/utils/error_reporter.py
@@ -14,59 +14,60 @@ class ErrorReporter:
     Generador de mensajes de error consistentes para el validador CSV
     """
 
-def generateFIeldError(rowNum: int, fieldName: str, errorType: str, details: str) -> List[str]:
-    """
-    Genera un mensaje de error para un campo específico
-    :param rowNum: numero de fila donde ocurrio el error
-    :param fieldName: nombre del campo con error
-    :param errorType: tipo de error (tipo_incorrecto, valor_nulo, etc.)
-    :param details: detalles adicionales sobre el error
-    :return: Array de strings con los mensajes de error
-    """
-    errors = list()
+    def generateFieldError(self, rowNum: int, fieldName: str, errorType: str, details: str) -> List[str]:
+        """
+        Genera un mensaje de error para un campo específico
+        :param rowNum: numero de fila donde ocurrio el error
+        :param fieldName: nombre del campo con error
+        :param errorType: tipo de error (tipo_incorrecto, valor_nulo, etc.)
+        :param details: detalles adicionales sobre el error
+        :return: Array de strings con los mensajes de error
+        """
+        errors = list()
 
-    if errorType == "tipo_incorrecto":
-        errorMsg = f"Fila {rowNum}: valor no {details} en columna '{fieldName}'"
-        errors.append(errorMsg)
-    elif errorType == "valor_nulo":
-        errorMsg = f"Fila {rowNum}: campo requerido '{fieldName}' está vacío"
-        errors.append(errorMsg)
-    elif errorType == "campo_faltante":
-        errorMsg = f"Archivo: campo requerido '{fieldName}' no encontrado en encabezados"
-        errors.append(errorMsg)
-    elif errorType == "campo_no_permitido":
-        errorMsg = f"Fila {rowNum}: campo '{fieldName}' no permitido según esquema"
-        errors.append(errorMsg)
-    else:
-        errorMsg = f"Fila {rowNum}: error en campo '{fieldName}' - {details}"
-        errors.append(errorMsg)
+        # TODO: ▼△▼△▼△▼△▼△▼△▼△▼△▼△ Refactorizar △▼△▼△▼△▼△▼△▼△▼△▼△▼
+        if errorType == "tipo_incorrecto":
+            errorMsg = f"Fila {rowNum}: valor no {details} en columna '{fieldName}'"
+            errors.append(errorMsg)
+        elif errorType == "valor_nulo":
+            errorMsg = f"Fila {rowNum}: campo requerido '{fieldName}' está vacío"
+            errors.append(errorMsg)
+        elif errorType == "campo_faltante":
+            errorMsg = f"Archivo: campo requerido '{fieldName}' no encontrado en encabezados"
+            errors.append(errorMsg)
+        elif errorType == "campo_no_permitido":
+            errorMsg = f"Fila {rowNum}: campo '{fieldName}' no permitido según esquema"
+            errors.append(errorMsg)
+        else:
+            errorMsg = f"Fila {rowNum}: error en campo '{fieldName}' - {details}"
+            errors.append(errorMsg)
+        return errors
 
-    return errors
+    def generateHeaderError(self, missingHeaders: List[str], unexpectedHeaders: List[str]) -> List[str]:
+        """
+        Genera mensajes de error para problemas con encabezados
+        :param unexpectedHeaders: Encabezados defectuosos
+        :return: Array de strings con los mensajes de error
+        """
+        errors = list()
+
+        for header in missingHeaders:
+            errors.extend(self.generateFieldError(
+                rowNum=0,
+                fieldName=header,
+                errorType="campo_faltante",
+                details="")
+            )
+
+        for header in unexpectedHeaders:
+            errorMsg = f"Archivo: Campo no esperado '{header}' encontrado en encabezados"
+            errors.append(errorMsg)
+        return errors
+
 
 # ▏▎▍▌▋▊▉▉▉▉▉▉▉▉ Pseudocódigo ▉▉▉▉▉▉▉▉▉▊▋▌▍▎▏
 
-public List < String > generateHeaderError(List < String > missingHeaders, List < String > unexpectedHeaders)
-"""
-Genera mensajes de error para problemas con encabezados
-"""
-var
-errors = list()
-
-for header in missingHeaders
-    errors.extend(this.generateFieldError(0, header, "campo_faltante", ""))
-
-for header in unexpectedHeaders
-    var
-    errorMsg = "archivo: campo no esperado '" + header + "' encontrado en encabezados"
-    errors.append(errorMsg)
-
-return errors
-
-public
-List < String > generateFileError(String
-fileName, String
-errorType, String
-details)
+public List < String > generateFileError(String fileName, String errorType, String details)
 """
 Genera mensajes de error relacionados con el archivo en sí
 """
