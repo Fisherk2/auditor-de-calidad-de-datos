dcd7636fd0790963bbc8ebc2d63c8ed6035d5f1e
Fisherk2 <fisher_k2@hotmail.com>
2026-02-11 19:40:45 -0600

    Define importacion de componentes principales

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
-# Archivo vacío para que Python reconozca el directorio como un paquete.
+"""
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+MÓDULO:      Clase
+AUTOR:       Fisherk2
+FECHA:       2026-02-11
+DESCRIPCIÓN: Package initialization for CSV Validator components
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+"""
+
+# Versión del paquete
+__version__ = "1.0.0"
+
+# Exportar componentes principales
+from .validators import CSVValidator
+from .readers import CSVReader
+
+# Definir qué se importa con "from package import *"
+__all__ = ["CSVValidator", "CSVReader"]
