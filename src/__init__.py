18d6d1252ed34b693062be28ceb5c4bed637d840
Fisherk2 <fisher_k2@hotmail.com>
2026-02-11 20:54:12 -0600

    src: Establece versionado y modulos disponibles para importacion

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,18 @@
 """
-■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-MÓDULO:      Init
-AUTOR:       Fisherk2
-FECHA:       2026-02-11
-DESCRIPCIÓN: Package initialization for CSV Validator components
-■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+Módulo src para el Validador de CSV para Pipelines de Datos
+Este paquete contiene todos los componentes necesarios para validar archivos CSV
+contra un esquema predefinido.
 """
 
 # ▁▂▃▄▅▆▇███████ Versión del paquete ███████▇▆▅▄▃▂▁ #
 __version__ = "1.0.0"
+__author__ = "Fisherk2"
 
-# ⋮⋮⋮⋮⋮⋮⋮⋮ Definir qué se importa con "from package import *" ⋮⋮⋮⋮⋮⋮⋮⋮ #
-from .validators import CSVValidator
-from .readers import CSVReader
-__all__ = ["CSVValidator", "CSVReader"]
+# ⋮⋮⋮⋮⋮⋮⋮⋮ Declaración de módulos disponibles para importación ⋮⋮⋮⋮⋮⋮⋮⋮ #
+__all__ = [
+    'CSVValidator',
+    'CSVReader',
+    'TypeValidator',
+    'SchemaValidator',
+    'ErrorReporter'
+]
