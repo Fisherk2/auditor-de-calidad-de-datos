67cc8d7d755ba1f93e4a5e287f2499b38dbeb203
Fisherk2 <fisher_k2@hotmail.com>
2026-02-20 03:14:27 -0600

    src: Establece declaracion de modulos disponibles para importacion de auditor de calidad de datos

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
 """
-Módulo src para el Validador de CSV para Pipelines de Datos
+Módulo src para el Validador de CSV y Auditor de Calidad de Datos
 Este paquete contiene todos los componentes necesarios para validar archivos CSV
-contra un esquema predefinido.
+contra un esquema predefinido y realizar análisis de calidad de datos.
 """
 
 # ▁▂▃▄▅▆▇███████ Versión del paquete ███████▇▆▅▄▃▂▁
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Fisherk2"
 
 # ⋮⋮⋮⋮⋮⋮⋮⋮ Importar clases desde submódulos ⋮⋮⋮⋮⋮⋮⋮⋮
@@ -15,11 +15,31 @@ from src.validators.type_validator import TypeValidator
 from src.validators.schema_validator import SchemaValidator
 from src.utils.csv_error_reporter import CSVErrorReporter
 
+from src.quality_auditor.main_auditor import QualityAuditor
+from src.quality_auditor.null_analyzer import NullAnalyzer
+from src.quality_auditor.uniqueness_analyzer import UniquenessAnalyzer
+from src.quality_auditor.statistical_analyzer import StatisticalAnalyzer
+from src.quality_auditor.date_analyzer import DateAnalyzer
+from src.readers.quality_rules_reader import QualityRulesReader
+from src.utils.quality_report import QualityReport
+from src.utils.data_parser import DataParser
+from src.utils.date_helper import DateHelper
+
 # ⋮⋮⋮⋮⋮⋮⋮⋮ Declaración de módulos disponibles para importación ⋮⋮⋮⋮⋮⋮⋮⋮
 __all__ = [
     'CSVValidator',
     'CSVReader',
     'TypeValidator',
     'SchemaValidator',
-    'CSVErrorReporter'
+    'CSVErrorReporter',
+
+    'QualityAuditor',
+    'NullAnalyzer',
+    'UniquenessAnalyzer',
+    'StatisticalAnalyzer',
+    'DateAnalyzer',
+    'QualityRulesReader',
+    'QualityReport',
+    'DataParser',
+    'DateHelper'
 ]
